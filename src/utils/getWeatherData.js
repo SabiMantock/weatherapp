45b6107b0e8139a4f0aba7b2eec7c7dd246512a8
SabiMantock <sabimantock@gmail.com>
2022-03-04 11:42:34 +0000

    http

--- src/utils/getWeatherData.js
+++ src/utils/getWeatherData.js
@@ -3,10 +3,10 @@ import axios from 'axios'
 const API_KEY = process.env.REACT_APP_API_KEY
 
 export const getWeatherData = async (fn1, fn2) => {
-  const tampere = `http://api.openweathermap.org/data/2.5/weather?id=634963&appid=${API_KEY}&units=metric`
-  const jyv = `http://api.openweathermap.org/data/2.5/weather?id=655195&appid=${API_KEY}&units=metric`
-  const kuo = `http://api.openweathermap.org/data/2.5/weather?id=650225&appid=${API_KEY}&units=metric`
-  const esp = `http://api.openweathermap.org/data/2.5/weather?id=660129&appid=${API_KEY}&units=metric`
+  const tampere = `https://api.openweathermap.org/data/2.5/weather?id=634963&appid=${API_KEY}&units=metric`
+  const jyv = `https://api.openweathermap.org/data/2.5/weather?id=655195&appid=${API_KEY}&units=metric`
+  const kuo = `https://api.openweathermap.org/data/2.5/weather?id=650225&appid=${API_KEY}&units=metric`
+  const esp = `https://api.openweathermap.org/data/2.5/weather?id=660129&appid=${API_KEY}&units=metric`
 
   const tampHourly = `https://api.openweathermap.org/data/2.5/onecall?lat=61.4991&lon=23.7871&exclude=daily,current,minutely&appid=${API_KEY}&units=metric`
   const jyvHourly = `https://api.openweathermap.org/data/2.5/onecall?lat=62.2081&lon=25.6074&exclude=daily,current,minutely&appid=${API_KEY}&units=metric`
